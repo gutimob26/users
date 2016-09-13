9336605b38d0fb10616e9ea16fd6af93bc3adc43
gutimob26 <gutimob26@gmail.com>
2016-09-13 11:42:38 -0600

    Update home.pp

--- manifests/home.pp
+++ manifests/home.pp
@@ -1,10 +1,7 @@
 #create a home dir for user elmo
-class home {
-user { 'elmo':
-  ensure     => 'present',
 
-file  { '/tmp/elmo':
-  ensure     => 'present',
-  managehome => true
+  file  { '/tmp/elmo':
+  ensure     => 'directory,
+  owner => => 'elmo',
+  gid => 'muppet',
   }
-}
